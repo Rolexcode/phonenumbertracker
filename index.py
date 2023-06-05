70b82817cf9a76412b584f06336ec67071e86af9
Olabode Olaleye <119955307+Rolexcode@users.noreply.github.com>
2023-06-05 22:27:30 +0100

    Update and rename index.py to requirements.txt

--- index.py
+++
@@ -1,7 +0,0 @@
-import phonenumbers
-from phonenumbers import geocoder, carrier, timezone
-number = input("Enter the phone number to be tracked: ")
-phone_number = phonenumbers.parse(number)
-print(f"Location: {geocoder.description_for_number(phone_number, 'en')}")
-print(f"Carrier: {carrier.name_for_number(phone_number, 'en')}")
-print(f"Time Zone: {timezone.time_zones_for_geographical_number(phone_number)}")
